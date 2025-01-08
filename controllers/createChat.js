b6096145dbb7a4124b8e36cd4d7f533ea428713a
Izanalgit <izanalvarez@gmail.com>
2025-01-08 12:39:27 +0100

    update : error mesages and sender on createChat controller

--- controllers/createChat.js
+++ controllers/createChat.js
@@ -1,19 +1,22 @@
 const { createSession} = require('../services/sessionsServices');
 const {sendInvitationEmail} = require('../services/resendServices');
+const {msgErr} = require('../utils/errorsMsg');
 
 module.exports = async (req, res) => {
     try {
         const { sender, emailRecipient } = req.body.payload;
 
         if (!sender || !emailRecipient) {
-            return res.status(400).json({ error: 'incorrect payload' });
+            return res
+                .status(400)
+                .json({ error: msgErr.errPayloadIncorrect});
         }
 
         // Crear sesión en memoria
         const session = createSession();
 
         // Enviar invitación por correo con Resend
-        await sendInvitationEmail(emailRecipient,session.chatToken,session.authTokens.user2);
+        await sendInvitationEmail(sender,emailRecipient,session.chatToken,session.authTokens.user2);
 
         res.json({
             message: 'Invitación enviada',
@@ -22,7 +25,9 @@ module.exports = async (req, res) => {
             keys: session.keys.user1,
         });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al crear el chat' });
+        msgErr.errConsole('create chat',error)
+        res
+            .status(500)
+            .json({ error: msgErr.errGeneral('send mail') });
     }
 };
