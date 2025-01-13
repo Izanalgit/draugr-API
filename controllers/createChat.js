938bf7dfc15924cab8ed795516a1aa4af5f3a698
Izanalgit <izanalvarez@gmail.com>
2025-01-13 11:09:44 +0100

    refact : response plus message added

--- controllers/createChat.js
+++ controllers/createChat.js
@@ -3,8 +3,17 @@ const {sendInvitationEmail} = require('../services/resendServices');
 const {msgErr} = require('../utils/errorsMsg');
 
 module.exports = async (req, res) => {
+
+    if (!req.body.payload) {
+        return res
+            .status(400)
+            .json({ error: msgErr.errPayloadRequired});
+    }
+
     try {
-        const { sender, emailRecipient } = req.body.payload;
+        const { sender, emailRecipient, message } = req.body.payload;
+
+        //payload needs validation
 
         if (!sender || !emailRecipient) {
             return res
@@ -12,22 +21,26 @@ module.exports = async (req, res) => {
                 .json({ error: msgErr.errPayloadIncorrect});
         }
 
-        // Crear sesión en memoria
+        // Create session on memory
         const session = createSession();
 
-        // Enviar invitación por correo con Resend
-        await sendInvitationEmail(sender,emailRecipient,session.chatToken,session.authTokens.user2);
+        // Send mail by Resend
+        await sendInvitationEmail(sender,emailRecipient,session.chatToken,session.authTokens.user2,message);
 
         res.json({
             message: 'Invitación enviada',
             chatToken: session.chatToken,
             authToken: session.authTokens.user1,
-            keys: session.keys.user1,
+            keys: {
+                user: session.keys.privateKey,
+                contact: session.keys.publicKeyRecipient,
+            },
         });
+
     } catch (error) {
         msgErr.errConsole('create chat',error)
         res
             .status(500)
-            .json({ error: msgErr.errGeneral('send mail') });
+            .json({ error: msgErr.errGeneral('sending invitation mail') });
     }
 };
