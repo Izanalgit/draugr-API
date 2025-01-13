4d478244350c925bb7f11d15eeea92c51f6492b0
Izanalgit <izanalvarez@gmail.com>
2025-01-13 13:15:31 +0100

    update : random secret on generate token util

--- utils/genToken.js
+++ utils/genToken.js
@@ -1,8 +1,9 @@
+const {tokenSecret} = require('../config/tokens');
 const jwt = require('jsonwebtoken');
 
 function genToken(payload,expires){
     const options = { expiresIn: expires, issuer: 'DraugrChat'};
-    return jwt.sign(payload,process.env.JWT_SECRET,options);
+    return jwt.sign(payload,tokenSecret,options);
 }
 
 module.exports = {genToken}
