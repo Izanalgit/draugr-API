63a4fcac5d68ddc47f7362c420e0f4e4eef5d875
Izanalgit <izanalvarez@gmail.com>
2025-01-13 13:14:16 +0100

    feat : random token secren on config

--- config/tokens.js
+++ config/tokens.js
@@ -1,4 +1,16 @@
-tokenConfig = {
+const crypto = require('crypto');
+
+function createSecret(){
+
+    const secret = crypto.randomBytes(64).toString('hex');
+    
+    return secret;
+}
+
+const tokenSecret = createSecret();
+
+const tokenConfig = {
+    tokenSecret,
     expireTime: '1h',
 };
 
