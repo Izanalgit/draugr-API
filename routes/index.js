799bf1029094d8972d799ab4bd22d02daeb27839
Izanalgit <izanalvarez@gmail.com>
2025-01-14 13:25:53 +0100

    feat : handshake route added on index routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use('/', require('./handshakeRoutes'));
 router.use('/', require('./inviteRoutes'));
 router.use('/', require('./loadRoutes'));
 
