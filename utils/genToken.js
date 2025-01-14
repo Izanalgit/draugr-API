61e58fceb818d5c299a86d25e91f1972208f8797
Izanalgit <izanalvarez@gmail.com>
2025-01-14 13:28:41 +0100

    update : token secret declaration deconstructed on genToken util

--- utils/genToken.js
+++ utils/genToken.js
@@ -1,9 +1,8 @@
-const {tokenSecret} = require('../config/tokens');
 const jwt = require('jsonwebtoken');
 
-function genToken(payload,expires){
+function genToken(payload,secret,expires){
     const options = { expiresIn: expires, issuer: 'DraugrChat'};
-    return jwt.sign(payload,tokenSecret,options);
+    return jwt.sign(payload,secret,options);
 }
 
 module.exports = {genToken}
