4ec7677662595c35854114f5c85466fdd9e547d1
Izanalgit <izanalvarez@gmail.com>
2025-01-15 14:23:55 +0100

    update : rate limit middleware on routes

--- routes/inviteRoutes.js
+++ routes/inviteRoutes.js
@@ -4,9 +4,17 @@ const createChat = require('../controllers/createChat');
 //Middlewares
 const authToken = require('../middleware/authCheck');
 const csrfTokenCheck = require('../middleware/csrfCheck');
+const ratesMiddleware = require('../middleware/rateLimit');
+const rateLimits = require('../config/rateLimits');
 
 const router = express.Router();
 
-router.post('/create', authToken, csrfTokenCheck, createChat);
+router.post(
+    '/create',
+    ratesMiddleware('handshake',rateLimits.handshake), 
+    authToken,  
+    csrfTokenCheck,
+    createChat
+);
 
 module.exports= router;
--- routes/loadRoutes.js
+++ routes/loadRoutes.js
@@ -4,9 +4,17 @@ const loadChat = require('../controllers/loadChat');
 //Middlewares
 const authToken = require('../middleware/authCheck');
 const csrfTokenCheck = require('../middleware/csrfCheck');
+const ratesMiddleware = require('../middleware/rateLimit');
+const rateLimits = require('../config/rateLimits');
 
 const router = express.Router();
 
-router.post('/load', authToken, csrfTokenCheck, loadChat);
+router.post(
+    '/load',
+    ratesMiddleware('handshake',rateLimits.handshake), 
+    authToken, 
+    csrfTokenCheck,
+    loadChat
+);
 
 module.exports= router;
