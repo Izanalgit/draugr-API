d501724a111f442dad8dc054ea31cee4ee95da90
Izanalgit <izanalvarez@gmail.com>
2025-01-16 11:48:19 +0100

    feat : accept chat validation

---
+++ validations/acceptValidation.js
@@ -0,0 +1,23 @@
+// JWT format
+const tokenFormat = (value) => {
+    if (!value.match(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)) {
+        throw new Error('El formato del Token es inválido');
+    }
+    return true;
+}
+
+const acceptChatValidation = [
+    body('payload.chatToken')
+        .trim()
+        .notEmpty()
+        .withMessage('Token de chat requerido')
+        .custom(tokenFormat),
+    body('payload.userToken')
+        .trim()
+        .notEmpty()
+        .withMessage('Token de user requerido')
+        .custom(tokenFormat)
+    
+]
+
+module.exports = { acceptChatValidation };
