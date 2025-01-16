300e2b41966d841e8523c6aa45d55ae9bb373908
Izanalgit <izanalvarez@gmail.com>
2025-01-16 11:45:34 +0100

    refact : error handler on cleanup routine

--- routines/cleanup.js
+++ routines/cleanup.js
@@ -1,18 +1,38 @@
 const { cleanRateLimiters } = require('../utils/rateLimiter');
 const { cleanExpiredSessions } = require('../services/sessionsServices');
 const { cleanHSMemory, cleanCSRFMemory } = require('../services/handshakeServices');
+const { msgErr } = require('../utils/errorsMsg');
 
 const CLEAN_INTERVAL = require('../config/cleanTime');
 
 const startCleanupRoutines = () => {
     setInterval(() => {
 
-        cleanRateLimiters(CLEAN_INTERVAL); 
-        cleanExpiredSessions();             
-        cleanHSMemory();             
-        cleanCSRFMemory();
+        try {
+            cleanRateLimiters(CLEAN_INTERVAL);
+        } catch (err) {
+            msgErr.errConsole('cleaning rate limiters:', err);
+        }
 
-        console.log('Clean memory routine...'); //CHIVATO ?
+        try {
+            cleanExpiredSessions();
+        } catch (err) {
+            msgErr.errConsole('cleaning expired sessions:', err);
+        }
+
+        try {
+            cleanHSMemory();
+        } catch (err) {
+            msgErr.errConsole('cleaning Handshake Memory:', err);
+        }
+
+        try {
+            cleanCSRFMemory();
+        } catch (err) {
+            msgErr.errConsole('cleaning CSRF Memory:', err);
+        }
+
+        console.log('Clean memory routine...'); //CHIVATO
         
     }, CLEAN_INTERVAL);
 };
