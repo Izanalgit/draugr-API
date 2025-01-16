056ab39023d7097a072bda19004acabbe05f1dbe
Izanalgit <izanalvarez@gmail.com>
2025-01-16 11:49:07 +0100

    feat : added validations on invite routes

--- routes/inviteRoutes.js
+++ routes/inviteRoutes.js
@@ -6,14 +6,20 @@ const authToken = require('../middleware/authCheck');
 const csrfTokenCheck = require('../middleware/csrfCheck');
 const ratesMiddleware = require('../middleware/rateLimit');
 const rateLimits = require('../config/rateLimits');
+const {validate} = require('../middleware/validations');
+
+//Validations
+const { inviteValidation } = require('../validations/inviteValidations');
 
 const router = express.Router();
 
 router.post(
     '/create',
-    ratesMiddleware('handshake',rateLimits.handshake), 
+    ratesMiddleware('handshake',rateLimits.handshake),
     authToken,  
     csrfTokenCheck,
+    inviteValidation,
+    validate, 
     createChat
 );
 
