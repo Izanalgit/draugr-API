4f978880efe134d7e148457dda850be592f989a4
Izanalgit <izanalvarez@gmail.com>
2025-01-16 11:47:26 +0100

    update : issuer check and error handler on decodeToken Util

--- utils/decodeToken.js
+++ utils/decodeToken.js
@@ -3,9 +3,25 @@ const jwt = require('jsonwebtoken');
 function decodeToken(token,secret) {
     try {
         const decoded = jwt.verify(token, secret);
-        return decoded; 
+
+        if (decoded.iss !== 'DraugrChat'){
+            console.warn('CAREFULL : INVALID ISSUER !!')
+            throw new Error('Invalid issuer!')
+        }
+
+        return decoded;
+         
     } catch (err) {
-        throw new Error(`Token decoded failed`);
+
+        if (err.name === 'JsonWebTokenError')
+            throw new Error(`JWT Error: ${err.message}`);
+
+        else if (err.name === 'TokenExpiredError')
+            throw new Error(`Token expired at: ${err.expiredAt}`);
+
+        else 
+            throw new Error(`Token decode failed: ${err.message}`);
+
     }
 }
 
