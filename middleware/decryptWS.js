2fa2abc9cb0a612188cb683f466971d8c112226e
Izanalgit <izanalvarez@gmail.com>
2025-01-30 20:22:59 +0100

    refact : decrypt ws middleware

--- middleware/decryptWS.js
+++ middleware/decryptWS.js
@@ -1,31 +1,36 @@
 const crypto = require('crypto');
 const { msgErr } = require('../utils/errorsMsg');
 
-const wsDecryptMiddleware = (socket, next) => {
+const wsDecryptMiddleware = (socket, handler) => {
     const { PRIVATE_KEY } = process.env;
 
     const privateKey = PRIVATE_KEY.split(String.raw`\n`).join('\n');
 
     socket.on('message', (encrypted) => {
         try {
-            const { encryptedData, encryptedKey, iv } = encrypted;
+            const { encryptedData, encryptedKey, iv } = JSON.parse(encrypted);
             
             const aesKey = crypto.privateDecrypt(privateKey, Buffer.from(encryptedKey, 'base64'));
     
             const decipher = crypto.createDecipheriv('aes-256-cbc', aesKey, Buffer.from(iv, 'base64'));
-            let decryptedData = decipher.update(Buffer.from(encryptedData, 'base64'));
-            decryptedData = Buffer.concat([decryptedData, decipher.final()]);
+            let decryptedMessage = decipher.update(Buffer.from(encryptedData, 'base64'));
+            decryptedMessage = Buffer.concat([decryptedMessage, decipher.final()]);
 
             const message = JSON.parse(decryptedMessage.toString('utf8'));
-            socket.emit('decryptedMessage', message);
+            console.log(message)//CHIVATO
+
+            handler(message, socket);
             
         } catch (err) {
             msgErr.errConsole('decrypt ws', err);
-            socket.emit('error', { error: 'Mensaje cifrado inválido' });
+            socket.send('error', { error: 'Invalid chiper message' });
         }
     });
 
-    next();
+    socket.on('error', (err) => {
+        msgErr.errConsole('Socket error catch', err.error);
+    });
+
 };
 
 module.exports = { wsDecryptMiddleware };
